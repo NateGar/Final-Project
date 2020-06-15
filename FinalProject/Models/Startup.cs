69fca74dcacd607f389932450678c9bc14ee49a9
Christina Lambert <clambert0615@gmail.com>
2020-06-15 13:27:54 -0400

    Added ratings method

--- FinalProject/Models/Startup.cs
+++ FinalProject/Models/Startup.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,11 +17,11 @@ namespace FinalProject.Models
     public class Record
     {
         public string id { get; set; }
-        public Fields fields { get; set; }
+        public Startups startups { get; set; }
         public DateTime createdTime { get; set; }
     }
 
-    public class Fields
+    public class Startups
     {
         [JsonProperty(PropertyName = "Company Name")]
         public string CompanyName { get; set; }
@@ -48,6 +49,114 @@ namespace FinalProject.Models
         public string Stage { get; set; }
         [JsonProperty(PropertyName = "State/Province")]
         public string StateProvince { get; set; }
+
+        public int Rating { get; set; }
+
+        public static void RateStartups(IEnumerable<Record> startups)
+        {
+            
+            int uniqueness;
+            int team;
+            int funding;
+            foreach(Record s in startups)
+            {
+                uniqueness = GetUniquenessRating(s);
+                team = GetTeamRating(s);
+                funding = GetFundingRating(s);
+
+                s.startups.Rating = uniqueness + team + funding;
+
+            }
+        }
+
+        public static int GetUniquenessRating(Record s)
+        {
+            int rating = 0;
+            int uniqueness = int.Parse(s.startups.Uniqueness);
+            if (uniqueness == 5)
+            {
+                rating = 3;
+            }
+            else if (uniqueness == 4)
+            {
+                rating = 5;
+            }
+            else if (uniqueness == 3)
+            {
+                rating = 4;
+            }
+            else if (uniqueness == 2)
+            {
+                rating = 2;
+            }
+            else if (uniqueness == 1)
+            {
+                rating = 1;
+
+            }
+            return rating;
+        }
+        public static int GetTeamRating(Record s)
+        {
+            int rating = 0;
+            int team = int.Parse(s.startups.Team);
+            if (team == 5)
+            {
+                rating = 5;
+            }
+            else if (team == 4)
+            {
+                rating = 4;
+            }
+           else if (team == 3)
+            {
+                rating = 3;
+            }
+            else if (team == 2)
+            {
+                rating = 2;
+            }
+            else if (team == 1)
+            {
+                rating = 1;
+            }
+
+            return rating;
+        }
+        public static int GetFundingRating(Record s)
+        {
+            int rating = 0;
+            int num;
+            if (int.TryParse(s.startups.Raised, NumberStyles.AllowThousands, CultureInfo.InvariantCulture, out num))
+            {
+                if (num > 5000000)
+                {
+                    rating = 5;
+                }
+                else if (num <= 5000000 && num > 2000000)
+                {
+                    rating = 4;
+                }
+                else if (num <= 2000000 && num > 1000000)
+                {
+                    rating = 3;
+                }
+                else if (num <= 1000000 && num > 500000)
+                {
+                    rating = 2;
+                }
+                else if (num <= 500000)
+                {
+                    rating = 1;
+                }
+            }
+            else
+            {
+                rating = 1;
+            }
+            return rating;
+        }
+
     }
 
 }
